7504652e4f07a5352a7e88224f2bacb15f98bfd3
Alec Keeler <alec.keeler@gmail.com>
2022-04-19 09:34:29 -0500

    test prompt complete

--- sql/debug_test_2.sql
+++ sql/debug_test_2.sql
@@ -2,9 +2,20 @@
 -- play Terraforming Mars.  We need to utilize our many-to-many relationship
 -- to find the correct names.  It should return the names in alphabetical order.
 
+-- Expected result:
+--   name  
+-- --------
+--  Alec
+--  Javier
+--  Rawaha
+-- (3 rows)
+
+-- Test the query using the following command:
+-- psql -d boardgame_dev < sql/debug_test_2
+
 SELECT players.name
 FROM players
-JOIN boardgames ON (lfg.game_id = boardgames.id)
+JOIN boardgames ON (lfg.game_id = boardgame.id)
 JOIN lfg ON (players.id = lfg.player_id)
 WHERE boardgames.name = 'Terraforming Mars'
 ORDER BY name;
