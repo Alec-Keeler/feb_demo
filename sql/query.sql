58db35cdef6e0a6c64f0f43f55465de4adac887d
Alec Keeler <alec.keeler@gmail.com>
2022-04-19 09:19:13 -0500

    w10d2 query exercise

---
+++ sql/query.sql
@@ -0,0 +1,14 @@
+-- Task 4g
+
+-- Write a query to the boardgames table that finds all board games, ordered by name.
+-- Then, reverse the order.  Then find only the alphabetically last game in the table.
+
+-- Expected result:
+--                name                
+-- -----------------------------------
+--  Twilight Imperium: Fourth Edition
+-- (1 row)
+
+-- Test your query by running the following command from your terminal in the root directory of the repo:
+-- psql -d boardgame_dev < sql/query.sql
+-- Your code below:
