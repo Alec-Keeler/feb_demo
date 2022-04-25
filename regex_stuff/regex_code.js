cb3b248a47e112b931f0b15c10a1780c15d11b98
Alec Keeler <alec.keeler@gmail.com>
2022-04-25 16:36:44 -0500

    w11d1

--- regex_stuff/regex_code.js
+++ regex_stuff/regex_code.js
@@ -1,1 +1,27 @@
 //for code
+// / forward slash
+// \ backslash
+
+const exp = /^\(?\d{3}\)?-?\d{3}-\d{4}$/
+
+function phoneNumCheck(string) {
+    // console.log(exp.test(string))
+    if (exp.test(string)) {
+        return true
+    } else {
+        return false
+    }
+}
+
+// phoneNumCheck('(123)456-7890')
+
+function changePhoneNum(string) {
+    const remove = /[()-]/g
+    const replace = ''
+
+    const newNum = string.replace(remove, replace)
+    console.log(newNum)
+    console.log(parseInt(newNum))
+}
+
+changePhoneNum('(123)456-7890')
