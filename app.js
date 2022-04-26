702ffa961f674700594b3e1fa8610caa47e2e058
Alec Keeler <alec.keeler@gmail.com>
2022-04-26 12:13:14 -0500

    tasks 19 20

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 // Task 17a
 const express = require('express');
-
+const { Breadditor } = require('./models');
 const app = express();
+// Task 19a
+app.set('view engine', 'pug')
 
 // Task 18
 app.get('/', (req, res) => {
-    res.send('Welcome to the home page')
+    // res.send('Welcome to the home page')
+    // Task 19c
+    res.render('home', {title: 'Breaddit', header: 'Welcome to Breaddit\'s Home Page'})
+})
+
+// Task 20a
+app.get('/users', async(req, res) => {
+    const breadditors = await Breadditor.findAll()
+
+    res.render('users', {title: 'Breaddit', breadditors: breadditors})
+})
+
+// /users/1
+// Task 20c
+app.get('/users/:id', async(req, res) => {
+    console.log(req.path)
+    console.log(req.params)
+    const user = await Breadditor.findByPk(req.params.id)
+    // console.log(user)
+    res.render('profile', {breadditor: user, title: 'Profile Page'})
 })
 
 
